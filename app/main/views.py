d8a70b9e6ac1046a8145e10ce0581651bf421d00
jacquline <jacqulinewangu@gmail.com>
2019-05-20 12:46:00 +0300

    added contect in the files

--- app/main/views.py
+++ app/main/views.py
@@ -4,3 +4,7 @@ from flask import render_template,url_for,redirect
 @main.route('/')
 def index():
     return render_template('index.html')
+
+@main.route('/headlines')
+def headlines():
+    return render_template('/headlines')
