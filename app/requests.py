c85024d19a3c8a6ee1ad2c0e8240fed3b8bfaa2a
jacquline <jacqulinewangu@gmail.com>
2019-05-21 14:43:07 +0300

    modified request.py added a search_url line on linew 10 to allow the search functionality.

--- app/requests.py
+++ app/requests.py
@@ -6,13 +6,16 @@ search_url = None
 news_url = None
 articles_url = None
 source_url = None
+news_by_source = None
+search_url = None
 
 def configure_request(app):
-    global api_key, search_url, news_url, articles_url,source_url
+    global api_key, search_url, news_url, articles_url, source_url, news_by_source
     api_key = app.config['API_KEY']
     news_url = app.config['ARTICLES_BASE_URL']
     source_url = app.config['SOURCES_BASE_URL']
     search_url = app.config['SEARCH_ARTICLE_BASE_URL']
+    news_by_source = app.config['NEW_BY_SOURCE']
 
 def get_sources():
     '''
@@ -22,6 +25,28 @@ def get_sources():
         response = []
         if data['sources']:
             response=process_sources(data['sources'])
+    return response
+
+
+def search_key(vals):
+    '''
+    '''
+    with urllib.request.urlopen(search_url.format(vals,api_key)) as url:
+        data = json.loads(url.read())
+        response = []
+        if data['articles']:
+            response = process_articles(data['articles'])
+    return response
+
+def get_source_news(source_id):
+    '''
+    '''
+    with urllib.request.urlopen(news_by_source.format(source_id,api_key)) as url:
+        data = json.loads( url.read() )
+        response = []
+        if data['articles']:
+            # print(data['articles'])
+            response = process_articles(data['articles'])
     return response
 
 def process_sources(sources_list):
@@ -43,15 +68,30 @@ def process_sources(sources_list):
 
     return results
 
+def get_articles():
+    '''
+    '''
+    with urllib.request.urlopen(news_url.format(api_key)) as url:
+        data = json.loads(url.read())
+        response = []
+        if data['articles']:
+            data_items=data['articles']
+            return process_articles(data_items)
+        else:
+            return []
+    
+
 def process_articles(articles_list):
     '''
     '''
-    results = []
+    results_list = []
     for item in articles_list:
-        id = item.get('id')
         title = item.get('title')
-        content = item.get('content')
-        image = item.get('image')
+        content = item.get('description')
         url = item.get('url')
-        urlToimage = item.get('urlToimage')
+        urlToImage = item.get('urlToImage')
         source = item.get('source')
+        if content and urlToImage:
+            article_object = Articles( title, content, url, urlToImage, source)
+            results_list.append(article_object)
+    return results_list
