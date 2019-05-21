06859b64bd46dc6cadd842b6a4950530c02368e7
jacquline <jacqulinewangu@gmail.com>
2019-05-21 14:46:05 +0300

    modify views.py added imports for the requests on line 3 bocause there was an error

--- app/main/views.py
+++ app/main/views.py
@@ -1,12 +1,35 @@
 from . import main
-from flask import render_template,url_for,redirect
-from ..requests import get_sources
+from flask import render_template,url_for,redirect,request
+from ..requests import get_sources, get_articles, get_source_news, search_key
 
 @main.route('/')
 def index():
     source = get_sources()
-    return render_template('index.html', sources = source)
+    search = request.args.get('news_query')
+    if search:
+        query = '+'.join( search.split(' ') )
 
-# @main.route('/headlines')
-# def headlines():
-#     return render_template('headlines.html')
+        return redirect( url_for('main.search', words = query ) )
+    else:
+        return render_template('index.html', sources = source)
+
+@main.route('/sources/<id>')
+def headlines(id):
+
+    articles = get_source_news(id)
+    search = request.args.get('news_query')
+    if search:
+        query = '+'.join( search.split(' ') )
+        return redirect( url_for('main.search', words = query ) )
+    else:    
+        return render_template('sources.html', article_data = articles)
+
+@main.route('/search/<words>')
+def search(words):
+    search_results = search_key(words)
+    search = request.args.get('news_query')
+    if search:
+        query = '+'.join( search.split(' ') )
+        return redirect( url_for('main.search', words = query ) )
+    else: 
+        return render_template('search.html', article_data = search_results)
